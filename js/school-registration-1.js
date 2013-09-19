62938c0e1e74cede84489a8705269a5337871a85
Zia ur Rehman <zia.pccr@yahoo.com>
2013-09-19 12:13:43 +0500

    JQuery Funtionality

--- js/school-registration-1.js
+++ js/school-registration-1.js
@@ -1,20 +1,41 @@
-jQuery(document).ready(function(){
-	addStaffMemberControl();	
-});
 
-function addStaffMemberControl()
-	{
-		jQuery("input[name='code-conduct-select']").change(function(){
-			if(jQuery(this).val() == "add-staff-member")
-			{
-				
-				jQuery(".ya-add-staff-member").slideDown();
-			}
-			else
-			{
-				jQuery(".ya-add-staff-member").slideUp();
-			}
-			
-		});
-	}
-
+jQuery(function(){
+	jQuery('#ower-option').on('click', function(){
+		if(jQuery(this).attr('checked') == 'checked'){
+			jQuery('.ya-add-ower-form').slideDown();
+			jQuery(".ya-add-staff-member").slideUp();
+		}
+		else{
+			jQuery('.ya-add-ower-form').hide();
+		}
+	});
+	
+	jQuery('#ower-option:checked').each(function(){
+		if(jQuery(this).attr('checked') == 'checked'){
+			jQuery('.ya-add-ower-form').css('display', 'block')
+		}
+		else{
+			jQuery('.ya-add-ower-form').css('display', 'none');
+		}
+	});
+	
+	jQuery('#add-staff-member-option').on('click', function(){
+		if(jQuery(this).attr('checked') == 'checked'){
+			jQuery('.ya-add-staff-member').slideDown();
+			jQuery(".ya-add-ower-form").slideUp();
+		}
+		else{
+			jQuery('.ya-add-staff-member').slideUp();
+		}
+	});
+	
+	jQuery('#add-staff-member-option:checked').each(function(){
+		if(jQuery(this).attr('checked') == 'checked'){
+			jQuery('.ya-add-staff-member').css('display', 'block');
+		}
+		else{
+			jQuery('.ya-add-staff-member').css('display', 'none');
+		}
+	});
+	
+})
